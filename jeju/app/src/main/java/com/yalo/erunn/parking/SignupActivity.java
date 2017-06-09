abef1157a8da0b8449fdab329b0a7a43ca9f6513
SangGeePark <erunningo@gmail.com>
2017-06-09 22:51:19 +0900

    comiit #7 : set a login activity
    set a login activity

---
+++ jeju/app/src/main/java/com/yalo/erunn/parking/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.yalo.erunn.parking;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by erunn on 2017-06-09.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+
+
+
+}
